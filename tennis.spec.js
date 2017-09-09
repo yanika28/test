f9a514e27114aa7c6d43f2c3d98c1be7ca112589
yanika <58160381@go.buu.ac.th>
2017-09-09 15:36:05 +0700

    tennistest

---
+++ tennis.spec.js
@@ -0,0 +1,19 @@
+function TennisGame() {
+  this.reset = () => {
+  }
+  this.echo = () => {
+    return 'Love-Love'
+  }
+}
+
+test('Echo "Love-Love" when game starts', () => {
+  //Arrange
+  let app = new TennisGame()
+  app.reset
+
+  //Act
+  let result = app.echo()
+
+  //Assert
+  expect(result).toBe('Love-Love')
+})
